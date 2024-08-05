9486b7ac833ba7985de851e030e185519396810c
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-08-05 22:45:39 +0530

    cetner text

--- components/landingPageComponents/SignupForm.tsx
+++ components/landingPageComponents/SignupForm.tsx
@@ -37,7 +37,7 @@ function SignupForm() {
   return (
     <div className="flex flex-col items-center  w-full min-h-[50vh] max-w-6xl justify-center   ">
       <div>
-        <p className="text-4xl font-bold mt-10">
+        <p className="text-4xl font-bold text-center mt-10">
           Signup to get notified when we release v1
         </p>
       </div>
