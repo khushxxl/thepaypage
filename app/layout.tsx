01cd7515f6b73509e85d4a0a2526ec84fda4b3be
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-08-05 23:13:59 +0530

    twtitter card added

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -26,6 +27,24 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <meta
+            name="twitter:card"
+            content="Create Payment Integration with Beautiful Pages in 3 minutes"
+          />
+          {/* <meta name="twitter:site" content="@nytimesbits" /> */}
+          <meta name="twitter:creator" content="@khushaal_04" />
+          <meta property="og:url" content="https://thepaypage.vercel.app" />
+          <meta property="og:title" content="💸 thepaypage" />
+          <meta
+            property="og:description"
+            content="Integrating payments has always been a problem 😮‍💨, now integrate stripe with just few lines of code, no more api calls or code errors! Just that? No. Get a customizable page which you can design with our no-code designer 🪄"
+          />
+          <meta
+            property="og:image"
+            content="https://i.ibb.co/cx3mN2r/Screenshot-2024-08-05-at-23-11-59.png"
+          />
+        </Head>
         <AppContextProvider>
           <body className={inter.className}>
             <Navbar />
